cffba2e85289556c79c95f4ed0c26298480035e2
RRFayad <renan.r.fayad@gmail.com>
2026-07-17 19:07:40 -0300

    feat: Add an agent with tavily feature

--- 3_reAct-search-agent/main.py
+++ 3_reAct-search-agent/main.py
@@ -3,17 +3,21 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
+from tavily import TavilyClient
 
 load_dotenv()
 
+tavily = TavilyClient()
+
 
 @tool
-def search_tool(query: str) -> str:
+def search_tool(query: str) -> dict:
     """
     This is a tool that searches for information based on a query. It takes a string input (the query) and returns a string output (the search results). In this example, it simply returns a hardcoded response for demonstration purposes.
     """
     print(f"Searching for: {query}")
-    return f"Tokyo weather is sunny"
+    response = tavily.search(query=query)
+    return response
 
 
 llm = ChatOllama(temperature=0, model="gpt-oss:latest")
@@ -24,7 +28,13 @@ agent = create_agent(model=llm, tools=tools)
 def main():
     print("Hello from 3-react-search-agent!")
     result = agent.invoke(
-        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+        {
+            "messages": [
+                HumanMessage(
+                    content="Search for 3 jobs, remote, in the US, for a software engineer, that requires both React and Langchain"
+                )
+            ]
+        }
     )
 
     print(result)
