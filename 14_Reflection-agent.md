c6dd51908f3d321e2f93b59f94db0befbc8fb1aa
RRFayad <renan.r.fayad@gmail.com>
2026-08-10 17:43:53 -0300

    feat: Add graph

--- 14_Reflection-agent.md
+++ 14_Reflection-agent.md
@@ -10,3 +10,10 @@
 2. Create system reflection prompt
 3. Create system generation prompt
 4. Create chain
+
+### Build Graph
+
+- Build graph with conditional edge for reflection and generation
+- **Obs.:**
+  - `add_messages` is a reducer to append messages to the graph
+  - We added `grandalf` to be able to use `print_ascii()`
--- 14_reflection-agent/main.py
+++ 14_reflection-agent/main.py
@@ -2,10 +2,55 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from typing import TypedDict, Annotated
 
-def main():
-    print("Hello from 14-reflection-agent!")
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import MessagesState, add_messages
+
+from chains import generate_chain, reflection_chain
+
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], "The messages in the conversation"]
+    # critique: Annotated[str, "The critique of the tweet"]
+    # new_tweet: Annotated[str, "The new tweet generated based on the critique"]
+
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: MessageGraph) -> MessageGraph:
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
+
+
+def reflection_node(state: MessageGraph) -> MessageGraph:
+    # Important: Add reflection messages as Human Messages
+    res = reflection_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+
+def should_continue(state: MessageGraph) -> str:
+    # Just to simulate a stopping condition, we will stop after 3 messages.
+    if len(state["messages"]) >= 3:
+        return END
+    return REFLECT
+
+
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+
+builder.set_entry_point(GENERATE)
+builder.add_conditional_edges(
+    GENERATE, should_continue, path_map={END: END, REFLECT: REFLECT}
+)
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().print_ascii())
 
 
 if __name__ == "__main__":
-    main()
+    print("Hello from 14-reflection-agent!")
