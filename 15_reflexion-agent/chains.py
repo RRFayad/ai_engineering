5101deb7aa3d5a7a4d41e7f2c73725be678867e4
RRFayad <renan.r.fayad@gmail.com>
2026-08-13 19:41:44 -0300

    feat: Add revisor agent

--- 15_reflexion-agent/chains.py
+++ 15_reflexion-agent/chains.py
@@ -11,7 +11,7 @@ from langchain_core.output_parsers.openai_tools import (
     PydanticToolsParser,
 )
 
-from schemas import AnswerQuestion
+from schemas import AnswerQuestion, ReviseAnswer
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 parser = JsonOutputToolsParser(return_id=True)
@@ -41,6 +41,19 @@ actor_prompt_template = ChatPromptTemplate.from_messages(
 first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Answer the question in detail, in about 250 words."
 )
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is concise and not more than 250 words.
+"""
+
+revisor = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     print("Hello from 15-reflexion-agent!")
--- 15_reflexion-agent/schemas.py
+++ 15_reflexion-agent/schemas.py
@@ -11,10 +11,20 @@ class Reflection(BaseModel):
 
 
 class AnswerQuestion(BaseModel):
+    """Answer the question"""
+
     answer: str = Field(description="˜250 word detailed answer to the question.")
     reflection: Reflection = Field(
         description="Reflection on the answer, including what is missing and what is superfluous."
     )
     search_queries: List[str] = Field(
         description="List of 1 - 3  search queries to research information and improve the answer addressing the critique."
+    )
+
+
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question"""
+
+    references: List[str] = Field(
+        description="Citations motivating your updated answer."
     )
